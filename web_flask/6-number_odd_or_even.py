29ccbaf8ae91cc60ce945230cfd0500fb9906980
Kwambokah <hellenkwam29@gmail.com>
2024-08-11 15:19:44 +0300

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,69 +1,58 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application."""
-from flask import Flask, render_template
+
+"""Script that starts a Flask web application
+listening on address 0.0.0.0 port 5000
+adds another route to serve /HBNB
+"""
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """
-    Route handler function for the root URL.
-    Return a given string
-    """
-    return "Hello HBNB!"
+def hello_hbnb() -> str:
+    """ Function that generates the main route """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Route handler function for '/hbnb' URL.
-    Returns a given string
-    """
-    return "HBNB"
+def do_hbnb() -> str:
+    """ Function that generates that serves hbhb"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cText(text):
-    """
-    Route handler function for '/c/<text>' URL.
-    Display C followed by the value of the text variable
-    """
-    return "C {}".format(text.replace("_", " "))
+def custom_url(text) -> str:
+    """ Function that generates that serves hbhb"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f'C {text}'
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythonText(text="is cool"):
-    """Display Python followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
+def custom_python_route(text):
+    # custom python view function
+    text = text.replace('_', ' ')
+    return f'Python {text}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def isNumber(n):
-    """Display “n is a number” only if n is an integer"""
-    if isinstance(n, int):
-        return "{} is a number".format(n)
+def custom_check_int(n):
+    # custom int view function
+    return f'{n} is a number'
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n=None):
-    """Display a HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
+def custom_template_page(n):
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n=None):
-    """Display a HTML page only if n is an integer:
-    H1 tag: “Number: n is even|odd” inside the tag BODY
-    """
-    if isinstance(n, int):
-        if n % 2:
-            eo = "odd"
-        else:
-            eo = "even"
-        return render_template("6-number_odd_or_even.html", n=n, eo=eo)
+def custom_template_even_odd(n):
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
